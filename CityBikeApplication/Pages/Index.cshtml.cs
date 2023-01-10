3b45155431b97bb27d45f2f8d806f29005f956bf
MikaKaiharju <mika.kaiharju@gmail.com>
2023-01-10 15:36:52 +0200

    Created async import for stations

--- CityBikeApplication/Pages/Index.cshtml.cs
+++ CityBikeApplication/Pages/Index.cshtml.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
 using System.Net;
+using System.Globalization;
 
 namespace CityBikeApplication.Pages
 {
     public class IndexModel : PageModel
     {
-        
+
+        // for debugging limit amount of lines to be read per data set
+        int limit = 50000;
+
         List<Journey> journeys = new List<Journey>();
         List<Station> stations = new List<Station>();
 
@@ -26,46 +30,59 @@ namespace CityBikeApplication.Pages
         string path3 = "C:\\Users\\Kaihiz\\Desktop\\DevAcademy\\2021-07.csv";
 
         // station data path
-        string path4 = "https://opendata.arcgis.com/datasets/726277c507ef4914b0aec3cbcfcbfafc_0.csv";
+        //string path4 = "https://opendata.arcgis.com/datasets/726277c507ef4914b0aec3cbcfcbfafc_0.csv";
+
+        string path4 = "C:\\Users\\Kaihiz\\Desktop\\DevAcademy\\Helsingin_ja_Espoon_kaupunkipyB6rA4asemat_avoin.csv";
 
         public async void OnGet()
         {
-
-            
             // create timestamp to calculate how long import took
             UpdateProgress("Import started");
             DateTime dt1 = DateTime.Now;
 
             // create async tasks for reading data
             List<Task> tasks = new List<Task>();
-            tasks.Add(GetListAsync(path1));
-            tasks.Add(GetListAsync(path2));
-            tasks.Add(GetListAsync(path3));
+            tasks.Add(GetJourneyListAsync(path1));
+            tasks.Add(GetJourneyListAsync(path2));
+            tasks.Add(GetJourneyListAsync(path3));
+            tasks.Add(GetStationListAsync(path4));
 
             await Task.WhenAll(tasks);
             foreach(Task task in tasks)
             {
-                journeys.AddRange(((Task<List<Journey>>) task).Result);
+                if (task.GetType().Equals(typeof(Task<List<Journey>>)))
+                {
+                    journeys.AddRange(((Task<List<Journey>>)task).Result);
+                }
+                if (task.GetType().Equals(typeof(Task<List<Station>>)))
+                {
+                    stations.AddRange(((Task<List<Station>>)task).Result);
+                }
             }
 
             // calculation of how long import took
             DateTime dt2 = DateTime.Now;
             TimeSpan ts = dt2.Subtract(dt1);
-            UpdateProgress("Import finished in " + (int) ts.TotalSeconds + " seconds with journey count of " + journeys.Count);
+            UpdateProgress("Import finished in " + (int) ts.TotalSeconds + " seconds with journey count of " + journeys.Count +
+                " and station count of " + stations.Count);
         }
 
-        public Task<List<Journey>> GetListAsync(string path)
+        public Task<List<Journey>> GetJourneyListAsync(string path)
         {
             return Task.Run(() => ImportJourneyData(path));
         }
+        public Task<List<Station>> GetStationListAsync(string path)
+        {
+            return Task.Run(() => ImportStationData(path));
+        }
+
 
         public List<Journey> ImportJourneyData(string path)
         {
             //  import journey data
             List<Journey> importedJourneys = new List<Journey>();
 
-            // for debugging limit amount of lines to be read
-            int limit = 50000;
+            // for debugging how many lines have been read
             int currentIteration = 0;
 
             // read data from url
@@ -123,6 +140,71 @@ namespace CityBikeApplication.Pages
 
             return importedJourneys;
         }
+
+        public List<Station> ImportStationData(string path)
+        {
+            List<Station> importedStations = new List<Station>();
+
+            // for debugging how many lines have been read
+            int currentIteration = 0;
+
+            // read data from url
+            //HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(path);
+            //HttpWebResponse httpWebResponse = (HttpWebResponse) httpWebRequest.GetResponse();
+
+            //using (StreamReader reader = new StreamReader(httpWebResponse.GetResponseStream()))
+            using (StreamReader reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream && currentIteration++ < limit)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(",");
+
+                    // 0 == FID
+                    // 1 == id
+                    // 2 == nimi
+                    // 3 == namn
+                    // 4 == name
+                    // 5 == osoite
+                    // 6 == address
+                    // 7 == kaupunki
+                    // 8 == stad
+                    // 9 == operaattori
+                    // 10 == kapasiteetti
+                    // 11 == x
+                    // 12 == y
+
+                    // parse coordinate strings to double (skip if can't be read for some reason)
+                    double x;
+                    try { x = double.Parse(values[11], CultureInfo.InvariantCulture); } catch (Exception e) { UpdateProgress("values[11]="+ values[11] + "e1=" + e.ToString()); continue; }
+                    double y;
+                    try { y = double.Parse(values[12], CultureInfo.InvariantCulture); } catch (Exception e) { UpdateProgress("values[12]=" + values[12] + "e2=" + e.ToString()); continue; }
+
+                    // since we got this far line of data should be validated
+                    // create new station class class and populate it
+                    Station station = new Station();
+                    station.id = values[1];
+                    station.nimi = values[2];
+                    station.namn = values[3];
+                    station.name = values[4];
+                    station.osoite = values[5];
+                    station.address = values[6];
+                    station.kaupunki = values[7];
+                    station.stad = values[8];
+                    station.operaattori = values[9];
+                    station.kapasiteetti = values[10];
+                    station.x = x;
+                    station.y = y;
+
+                    // add station to station list
+                    importedStations.Add(station);
+                }
+            }
+
+            return importedStations;
+
+        }
+
 
         public void UpdateProgress(string s)
         {
--- CityBikeApplication/Station.cs
+++ CityBikeApplication/Station.cs
@@ -8,6 +8,16 @@ namespace CityBikeApplication
     public class Station
     {
         public string id;
-        public string stationName;
+        public string nimi;
+        public string namn;
+        public string name;
+        public string osoite;
+        public string address;
+        public string kaupunki;
+        public string stad;
+        public string operaattori;
+        public string kapasiteetti;
+        public double x;
+        public double y;
     }
 }
